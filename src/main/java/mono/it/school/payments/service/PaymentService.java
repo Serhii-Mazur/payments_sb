fee4a7107183d4970ba3292742ccd4882d2c66bd
serhii_mazur <ecm.mazur@gmail.com>
2022-05-04 08:30:51 +0300

    PaymentService extended: getByStatus method added

--- src/main/java/mono/it/school/payments/service/PaymentService.java
+++ src/main/java/mono/it/school/payments/service/PaymentService.java
@@ -2,6 +2,10 @@ package mono.it.school.payments.service;
 
 import mono.it.school.payments.domain.Payment;
 
+import java.util.List;
+
 public interface PaymentService {
     void save(Payment payment);
+
+    List<Payment> getByStatus(String status);
 }
